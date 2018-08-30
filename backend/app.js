3dcc295df98d6e4fbd0efe9f8a942d0f51dde758
Yunhsiu Li <anita8004@gmail.com>
2018-08-30 17:47:43 +0800

    後端修改

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-const server = app.listen(3001, function () {
+const server = app.listen(process.env.PORT || 3001, '0.0.0.0', function () {
   console.log('server running on port 3001');
 });
 
