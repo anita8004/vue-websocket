00b6670cbcd93adda81ebd1463d75469ef2e586a
Yunhsiu Li <anita8004@gmail.com>
2018-08-30 16:46:21 +0800

    vue-websocket

---
+++ src/components/Chat2.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="websocket-chat">
+    <div class="container">
+      <form id="getName">
+        <input type="text" name="name">
+        <button type="submit">SEND</button>
+      </form>
+      <div id="chatRoom">
+        <div class="users">
+          <h3 class="listName">USERS</h3>
+          <ul>
+            <!-- <li> {{user}} </li> -->
+          </ul>
+        </div>
+        <div class="chatBox">
+          <div class="chatBoard"></div>
+          <form class="chatForm">
+            <input type="text">
+            <button type="submit">↩</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Chat',
+  data () {
+    return {
+      users: [],
+      messages: {},
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+import App from './App'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  components: { App },
+  template: '<App/>'
+})
